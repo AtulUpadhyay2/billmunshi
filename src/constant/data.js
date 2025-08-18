7ec8a6943157808324eb1a62662b4b1332bc1c46
AtulUpadhyay1 <atulupadhyay50003@gmail.com>
2025-08-18 23:46:12 +0530

    Refactor menuItems structure by removing unused entries and organizing existing items for clarity

--- src/constant/data.js
+++ src/constant/data.js
@@ -7,488 +7,127 @@ export const menuItems = [
   {
     title: "Dashboard",
     icon: "heroicons-outline:home",
-    isOpen: true,
     isHide: true,
-    child: [
-      {
-        childtitle: "Analytics Dashboard",
-        childlink: "dashboard",
-      },
-      {
-        childtitle: "Ecommerce Dashboard",
-        childlink: "ecommerce",
-      },
-      {
-        childtitle: "Project  Dashbaord",
-        childlink: "project",
-      },
-      {
-        childtitle: " CRM Dashbaord",
-        childlink: "crm",
-      },
-      {
-        childtitle: "Banking Dashboard",
-        childlink: "banking",
-      },
-    ],
-  },
-  {
-    title: "changelog",
-    icon: "heroicons:arrow-trending-up",
-    link: "changelog",
-    isHide: false,
-    badge: "1.0.0",
+    link: "dashboard",
   },
   {
     isHeadr: true,
-    title: "apps",
+    title: "Zoho",
   },
 
   {
-    title: "Chat",
+    title: "Vendor Bill",
     isHide: true,
-    icon: "heroicons-outline:chat",
-    link: "chat",
-  },
-
-  {
-    title: "Email",
-    isHide: true,
-    icon: "heroicons-outline:mail",
-    link: "email",
-  },
-
-  {
-    title: "Kanban",
-    isHide: true,
-    icon: "heroicons-outline:view-boards",
-    link: "kanban",
-  },
-  {
-    title: "Calender",
-    isHide: true,
-    icon: "heroicons-outline:calendar",
-    link: "calender",
-  },
-
-  {
-    title: "Todo",
-    isHide: true,
-    icon: "heroicons-outline:clipboard-check",
-    link: "todo",
-  },
-
-  {
-    title: "Projects",
     icon: "heroicons-outline:document",
     link: "#",
+  },
+
+  {
+    title: "Expense Bill",
+    isHide: true,
+    icon: "heroicons-outline:document",
+    link: "#",
+  },
+
+  {
+    title: "Config",
+    icon: "heroicons-outline:cog",
+    link: "#",
     isHide: true,
     child: [
       {
-        childtitle: "Projects",
-        childlink: "projects",
+        childtitle: "Credentials",
+        childlink: "credentials",
       },
       {
-        childtitle: "Project Details",
-        childlink: "project-details",
+        childtitle: "Chart of account",
+        childlink: "chart-of-account",
       },
+      {
+        childtitle: "Taxes",
+        childlink: "taxes",
+      },
+      {
+        childtitle: "TDS / TCS",
+        childlink: "tds-tcs",
+      },
+      {
+        childtitle: "Vendors",
+        childlink: "vendors",
+      },
+      {
+        childtitle: "Vendors Credits",
+        childlink: "vendors-credits",
+      }
     ],
   },
-  {
-    title: "Ecommerce",
-    icon: "heroicons:shopping-bag",
-    link: "#",
-    child: [
-      {
-        childtitle: "User App",
-
-        multi_menu: [
-          {
-            multiTitle: "Products",
-            multiLink: "products",
-          },
-          {
-            multiTitle: "Products Details",
-            multiLink: "products/1",
-          },
-          {
-            multiTitle: "Cart",
-            multiLink: "cart",
-          },
-          {
-            multiTitle: "Wishlist",
-            multiLink: "wishlist",
-          },
-        ],
-      },
-      {
-        childtitle: "Admin App",
-
-        multi_menu: [
-          {
-            multiTitle: "Orders",
-            multiLink: "orders",
-            badge: "soon",
-          },
-          {
-            multiTitle: "Add Product",
-            multiLink: "add-product",
-            badge: "soon",
-          },
-          {
-            multiTitle: "Edit Product",
-            multiLink: "edit-product",
-            badge: "soon",
-          },
-          {
-            multiTitle: "Customers",
-            multiLink: "customers",
-            badge: "soon",
-          },
-          {
-            multiTitle: "Sellers",
-            multiLink: "sellers",
-            badge: "soon",
-          },
-          {
-            multiTitle: "Invoice",
-            multiLink: "invoice-ecommerce",
-            badge: "soon",
-          },
-        ],
-      },
-    ],
-  },
+  
   {
     isHeadr: true,
-    title: "Pages",
+    title: "Tally",
   },
+
   {
-    title: "Authentication",
+    title: "Vendor Bill",
+    isHide: true,
+    icon: "heroicons-outline:document",
+    link: "vendor-bill",
+  },
+
+  {
+    title: "Expense Bill",
+    isHide: true,
+    icon: "heroicons-outline:document",
+    link: "expense-bill",
+  },
+
+  {
+    title: "Config",
+    icon: "heroicons-outline:cog",
+    link: "#",
+    isHide: true,
+    child: [
+      {
+        childtitle: "Credentials",
+        childlink: "credentials",
+      }
+    ],
+  },
+
+  {
+    isHeadr: true,
+    title: "Settings",
+  },
+
+  {
+    title: "Api Keys",
+    isHide: true,
     icon: "heroicons-outline:lock-closed",
-    link: "#",
-    child: [
-      {
-        childtitle: "Signin One",
-        childlink: "/",
-      },
-      {
-        childtitle: "Signin Two",
-        childlink: "/login2",
-      },
-      {
-        childtitle: "Signin Three",
-        childlink: "/login3",
-      },
-      {
-        childtitle: "Signup One",
-        childlink: "/reg",
-      },
-      {
-        childtitle: "Signup Two",
-        childlink: "/reg2",
-      },
-      {
-        childtitle: "Signup Three",
-        childlink: "/reg3",
-      },
-      {
-        childtitle: "Forget Password One",
-        childlink: "/forgot-password",
-      },
-      {
-        childtitle: "Forget Password Two",
-        childlink: "/forgot-password2",
-      },
-      {
-        childtitle: "Forget Password Three",
-        childlink: "/forgot-password3",
-      },
-      {
-        childtitle: "Lock Screen One",
-        childlink: "/lock-screen",
-      },
-      {
-        childtitle: "Lock Screen Two",
-        childlink: "/lock-screen2",
-      },
-      {
-        childtitle: "Lock Screen Three",
-        childlink: "/lock-screen3",
-      },
-    ],
+    link: "api-keys",
   },
-  {
-    title: "Utility",
-    icon: "heroicons-outline:view-boards",
-    link: "#",
-    isHide: false,
-    child: [
-      {
-        childtitle: "Invoice",
-        childlink: "invoice",
-      },
-      {
-        childtitle: "Pricing",
-        childlink: "pricing",
-      },
-      {
-        childtitle: "FAQ",
-        childlink: "faq",
-      },
-      {
-        childtitle: "Blog",
-        childlink: "blog",
-      },
-      {
-        childtitle: "Blank page",
-        childlink: "blank-page",
-      },
-      {
-        childtitle: "Prfoile",
-        childlink: "profile",
-      },
-      {
-        childtitle: "Settings",
-        childlink: "settings",
-      },
-      {
-        childtitle: "404 page",
-        childlink: "/404",
-      },
 
-      {
-        childtitle: "Coming Soon",
-        childlink: "/coming-soon",
-      },
-      {
-        childtitle: "Under Maintanance page",
-        childlink: "/under-construction",
-      },
-    ],
-  },
   {
-    isHeadr: true,
-    title: "Elements",
+    title: "Members",
+    isHide: true,
+    icon: "heroicons-outline:users",
+    link: "members",
   },
+
   {
-    title: "Widgets",
-    icon: "heroicons-outline:view-grid-add",
-    link: "#",
-    child: [
-      {
-        childtitle: "Basic",
-        childlink: "basic",
-      },
-      {
-        childtitle: "Statistic",
-        childlink: "statistic",
-      },
-    ],
+    title: "Profile",
+    isHide: true,
+    icon: "heroicons-outline:user",
+    link: "profile",
   },
+
   {
-    title: "Components",
-    icon: "heroicons-outline:collection",
-    link: "#",
-    child: [
-      {
-        childtitle: "Typography",
-        childlink: "typography",
-      },
-      {
-        childtitle: "Colors",
-        childlink: "colors",
-      },
-      {
-        childtitle: "Alert",
-        childlink: "alert",
-      },
-      {
-        childtitle: "Button",
-        childlink: "button",
-      },
-      {
-        childtitle: "Card",
-        childlink: "card",
-      },
-      {
-        childtitle: "Carousel",
-        childlink: "carousel",
-      },
-      {
-        childtitle: "Dropdown",
-        childlink: "dropdown",
-      },
-      {
-        childtitle: "Image",
-        childlink: "image",
-      },
-      {
-        childtitle: "Modal",
-        childlink: "modal",
-      },
-      {
-        childtitle: "Progress bar",
-        childlink: "progress-bar",
-      },
-      {
-        childtitle: "Placeholder",
-        childlink: "placeholder",
-      },
-      {
-        childtitle: "Tab & Accordion",
-        childlink: "tab-accordion",
-      },
-      {
-        childtitle: "Badges",
-        childlink: "badges",
-      },
-      {
-        childtitle: "Paginatins",
-        childlink: "paginations",
-      },
-      {
-        childtitle: "Video",
-        childlink: "video",
-      },
-      {
-        childtitle: "Tooltip & Popover",
-        childlink: "tooltip-popover",
-      },
-    ],
+    title: "Subscriptions",
+    isHide: true,
+    icon: "heroicons-outline:credit-card",
+    link: "pricing",
   },
-  {
-    title: "Forms",
-    icon: "heroicons-outline:clipboard-list",
-    link: "#",
-    child: [
-      {
-        childtitle: "Input",
-        childlink: "input",
-      },
-      {
-        childtitle: "Input group",
-        childlink: "input-group",
-      },
-      {
-        childtitle: "Input layout",
-        childlink: "input-layout",
-      },
-      {
-        childtitle: "Form validation",
-        childlink: "form-validation",
-      },
-      {
-        childtitle: "Wizard",
-        childlink: "form-wizard",
-      },
-      {
-        childtitle: "Input mask",
-        childlink: "input-mask",
-      },
-      {
-        childtitle: "File input",
-        childlink: "file-input",
-      },
-      {
-        childtitle: "Form repeater",
-        childlink: "form-repeater",
-      },
-      {
-        childtitle: "Textarea",
-        childlink: "textarea",
-      },
-      {
-        childtitle: "Checkbox",
-        childlink: "checkbox",
-      },
-      {
-        childtitle: "Radio button",
-        childlink: "radio-button",
-      },
-      {
-        childtitle: "Switch",
-        childlink: "switch",
-      },
-      {
-        childtitle: "Select & Vue select",
-        childlink: "select",
-      },
-      {
-        childtitle: "Date time picker",
-        childlink: "date-time-picker",
-      },
-    ],
-  },
-  {
-    title: "Table",
-    icon: "heroicons-outline:table",
-    link: "#",
-    child: [
-      {
-        childtitle: "Basic Table",
-        childlink: "table-basic",
-      },
-      {
-        childtitle: "React Table",
-        childlink: "react-table",
-      },
-    ],
-  },
-  {
-    title: "Chart",
-    icon: "heroicons-outline:chart-bar",
-    link: "#",
-    child: [
-      {
-        childtitle: "Apex chart",
-        childlink: "appex-chart",
-      },
-      {
-        childtitle: "Chart js",
-        childlink: "chartjs",
-      },
-      {
-        childtitle: "Recharts",
-        childlink: "recharts",
-      },
-    ],
-  },
-  {
-    title: "Map",
-    icon: "heroicons-outline:map",
-    link: "map",
-  },
-  {
-    title: "Icons",
-    icon: "heroicons-outline:emoji-happy",
-    link: "icons",
-  },
-  {
-    title: "Multi Level",
-    icon: "heroicons:share",
-    link: "#",
-    child: [
-      {
-        childtitle: "Level 1.1",
-        childlink: "icons",
-      },
-      {
-        childtitle: "Level 1.2",
-        childlink: "Level-1",
-        multi_menu: [
-          {
-            multiTitle: "Level 2.1",
-            multiLink: "Level-2",
-          },
-          {
-            multiTitle: "Level 2.2",
-            multiLink: "Level-2.3",
-          },
-        ],
-      },
-    ],
-  },
+
+
 ];
 
 export const topMenu = [
